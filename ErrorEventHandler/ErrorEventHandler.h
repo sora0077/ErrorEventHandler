e534e06b3e0175cee64f9859affa29d25104f0b4
林達也 <t.hayashi0077+github@gmail.com>
2016-08-20 15:52:48 +0900

    Initial Commit

---
+++ ErrorEventHandler/ErrorEventHandler.h
@@ -0,0 +1,19 @@
+//
+//  ErrorEventHandler.h
+//  ErrorEventHandler
+//
+//  Created by 林達也 on 2016/08/20.
+//  Copyright © 2016年 jp.sora0077. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ErrorEventHandler.
+FOUNDATION_EXPORT double ErrorEventHandlerVersionNumber;
+
+//! Project version string for ErrorEventHandler.
+FOUNDATION_EXPORT const unsigned char ErrorEventHandlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ErrorEventHandler/PublicHeader.h>
+
+
